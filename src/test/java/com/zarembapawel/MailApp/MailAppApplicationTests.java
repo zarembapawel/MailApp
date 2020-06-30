80cc51404772b69a37c2425cd7a76f5008afc321
p.zaremba <p.zaremba@ultimate.systems>
2020-06-30 12:36:33 +0200

    add spring boot

---
+++ src/test/java/com/zarembapawel/MailApp/MailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarembapawel.MailApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
